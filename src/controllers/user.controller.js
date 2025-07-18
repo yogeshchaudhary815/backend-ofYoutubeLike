a6073351e2e13031b3bb1edaef4046e1cec84c14
yogesh chaudhary <yogeshchaudhary815>
2025-07-18 09:56:59 +0530

    fix: correct variable name typo and improve cover image upload handling in user registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 
-const registerUser = asyncHandler( async (requestAnimationFrame, res) => {
+const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
     // validation - not empty
     // check if user already exists : username , email
@@ -26,23 +26,30 @@ const registerUser = asyncHandler( async (requestAnimationFrame, res) => {
     // another method but same work 
     
     if(
-         [fullNmae, email, username, password].some((field) => 
+         [fullName, email, username, password].some((field) => 
             field?.trim() === "")
     ) {
          // if true
          throw new ApiError(400,"all fields are required")
     }
 
-   const existedUser = User.findOne({
+   const existedUser = await User.findOne({
         $or: [{ username },{ email }]
     })
 
     if(existedUser) {
         throw new ApiError(409,"User with email or username already existsss")
     }
+    // console.log(req.files)
 
     const avatarLocalPath = req.files?.avatar[0]?.path // multer mai check karo ye sab multer ki bajha se hua hai
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;   // use another method because this method shows error if cover image not upload
+    
+    let coverImageLocalPath;
+    if( req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+       coverImageLocalPath = req.files.coverImage[0].path
+    }
+    
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar is required")
@@ -51,7 +58,7 @@ const registerUser = asyncHandler( async (requestAnimationFrame, res) => {
    const avatar = await uploadOnCloudinary(avatarLocalPath)
    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-   if(!coverImage) {
+   if(!avatar) {
     throw new ApiError(400, "avatar is reqired..")
    }
 
