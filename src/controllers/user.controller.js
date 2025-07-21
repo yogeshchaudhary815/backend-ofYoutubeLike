3a76c598f558f28253e9ec39b2e96a3ec756b075
yogesh chaudhary <yogeshchaudhary815>
2025-07-21 14:18:28 +0530

    feat: add getWatchHistory function to retrieve user's watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt  from "jsonwebtoken";
 import { json } from "express";
 import { subscribe } from "diagnostics_channel";
+import mongoose from "mongoose";
 const generateAccessAndRefreshToken = async(userId) => {
   try {
     const user = await User.findById(userId)
@@ -415,6 +416,61 @@ const getUserChannelProfile = asyncHandler( async(req, res) => {
   )
 })
 
+const getWatchHistory = asyncHandler( async(req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "user",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+           $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      user[0].watchHistory,
+      "watch History fetched successfully "
+    )
+  )
+})
+
+
 export {
   registerUser,
   logoutUser,
@@ -424,5 +480,6 @@ export {
   getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
-  updateUserCoverImage
+  updateUserCoverImage,
+  getWatchHistory
 }
